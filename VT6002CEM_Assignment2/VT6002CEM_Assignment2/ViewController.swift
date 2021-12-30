604dd2f485fce0b1d2d952f0f020ef5d3d0a883e
ansonng0401 <ansonng0401@gmail.com>
2021-12-30 19:10:09 +0800

    Create Project

---
+++ VT6002CEM_Assignment2/VT6002CEM_Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VT6002CEM_Assignment2
+//
+//  Created by Anson on 30/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
