7c1e1ae552a70747ef9611f46c9d9ab40e77ce85
ansonng0401 <32608511+ansonng0401@users.noreply.github.com>
2022-01-04 02:56:22 +0800

    Create login functions and login signin ui update
    Create login functions and login signin ui update

--- VT6002CEM_Assignment2/VT6002CEM_Assignment2/View Controllers/LoginViewController.swift
+++ VT6002CEM_Assignment2/VT6002CEM_Assignment2/View Controllers/LoginViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
@@ -14,7 +14,7 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginButton: UIButton!
     
-    @IBOutlet weak var errorLabel: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,16 +28,25 @@ class LoginViewController: UIViewController {
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginTapped(_ sender: Any) {
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+    
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if error != nil {
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+            }
+            else {
+                
+                let mainPageViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.mainPageViewController) as? MainPageViewController
+                
+                self.view.window?.rootViewController = mainPageViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+        }
     }
-//    */
-//    @IBAction func loginTapped(_ sender: Any) {
-//    }
     
 }
+
