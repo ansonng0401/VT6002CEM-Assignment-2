3c9b3f29e8165fd9e69947d7d7298b3c1ead3f2b
ansonng0401 <32608511+ansonng0401@users.noreply.github.com>
2022-01-09 21:02:05 +0800

    update table view
    test the first table view data

--- VT6002CEM_Assignment2/VT6002CEM_Assignment2/View Controllers/MainMilkShopInfoViewController.swift
+++ VT6002CEM_Assignment2/VT6002CEM_Assignment2/View Controllers/MainMilkShopInfoViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
-var placeImages = ["1","2","3","4","5"]
-var placeNames = ["1","2","3","4","5"]
-var placeDesc = ["1","2","3","4","5"]
+var placeNames = ["蘭芳園 Lan Fong Yuen","2","3","4","5"]
+var placeAddress=["2 Gage St, Central","2"]
+var placeTel=["2544 3895","2"]
+var placeBusinehours=["(Monday to Saturday)07:30am-18:00pm","2"]
+var placeImages=["HK_Lan_Fong_Yuen_Old_Shop.jpeg","2"]
+var placeDesc = ["Lan Fong Yuan, which opened in 1952, operated as a food stall in the early years. Although it now has its main store in Central, Sheung Wan and two branches in Tsim Sha Tsui, the iron-clad stall on the street still remains to this day. Lanfangyuan is the first to use silk stockings to brew milk tea. The tea is brewed with post-season tea leaves from Colombo, Sri Lanka and non-dairy milk from Malaysia. The mouth is smooth and the tea fragrance is sweet. Over the years, it has attracted countless tourists and Hong Kong people to patronize, becoming a famous brand representative in the milk tea industry","2","3","4","5"]
+var placegooglemapurl=["https://www.google.com.hk/maps/place/%E8%98%AD%E8%8A%B3%E5%9C%92/@22.3079123,114.2521856,15z/data=!4m5!3m4!1s0x3404007b6fb3ed51:0x3f55838a3a330da3!8m2!3d22.2827255!4d114.1537155","2"]
 
 var myIndex=0
 
@@ -19,20 +23,8 @@ class MainMilkShopInfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension MainMilkShopInfoViewController:UITableViewDelegate,UITableViewDataSource{
@@ -53,5 +45,6 @@ extension MainMilkShopInfoViewController:UITableViewDelegate,UITableViewDataSour
         myTable.deselectRow(at: indexPath, animated: true)
         performSegue(withIdentifier: "MilkShopInfo", sender: self)
     }
+    
     
 }
--- VT6002CEM_Assignment2/VT6002CEM_Assignment2/View Controllers/MilkShopInfoViewController.swift
+++ VT6002CEM_Assignment2/VT6002CEM_Assignment2/View Controllers/MilkShopInfoViewController.swift
@@ -6,23 +6,38 @@
 //
 
 import UIKit
+import FirebaseMLModelDownloader
 
 class MilkShopInfoViewController: UIViewController {
 
    
-    @IBOutlet weak var placeDescriptionLable: UILabel!
+
+    @IBOutlet weak var placeDescriptionTextView: UITextView!
+ 
     @IBOutlet weak var placeImage: UIImageView!
     @IBOutlet weak var placeNameLabel: UILabel!
+    
+    @IBOutlet weak var placeAddressLabel: UILabel!
+    
+    @IBOutlet weak var PlaceBusinehoursLabel: UILabel!
+    
+    @IBOutlet weak var placeTelLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         placeNameLabel.text=placeNames[myIndex]
-        //placeImage.image = UIImage(name: placeImages[myIndex])
-        placeDescriptionLable.text=placeDesc[myIndex]
+        placeImage.image = UIImage(named: placeImages[myIndex])
+        placeDescriptionTextView.text=placeDesc[myIndex]
+        placeAddressLabel.text=placeAddress[myIndex]
+        placeTelLabel.text=placeTel[myIndex]
+        PlaceBusinehoursLabel.text=placeBusinehours[myIndex]
 
-        // Do any additional setup after loading the view.
+
     }
     
 
+    @IBAction func urlButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string:placegooglemapurl[myIndex])! as URL, options: [:], completionHandler:nil)
+    }
     /*
     // MARK: - Navigation
 
