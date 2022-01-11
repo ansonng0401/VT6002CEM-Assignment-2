a1fb4bc97b84c5c7d40aef55e9865988b5bcd26c
ansonng0401 <32608511+ansonng0401@users.noreply.github.com>
2022-01-11 17:32:28 +0800

    FINISH IMAGE PACKER FUNCTION

--- VT6002CEM_Assignment2/VT6002CEM_Assignment2/View Controllers/SettingPageViewController.swift
+++ VT6002CEM_Assignment2/VT6002CEM_Assignment2/View Controllers/SettingPageViewController.swift
@@ -10,8 +10,7 @@ import FirebaseAuth
 import Firebase
 
 class SettingPageViewController: UIViewController {
-    
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,9 +40,30 @@ class SettingPageViewController: UIViewController {
     }
     
     @IBAction func didTapButton(){
-        
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
     }
+
 }
+
+extension SettingPageViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+    
+    
+    if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+        imageView.image = image
+      }
+      picker.dismiss(animated: true, completion: nil)
+      }
+      
+      func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+      picker.dismiss(animated: true, completion: nil)
+      }
+      }
 
 
 
